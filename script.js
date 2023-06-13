db42b91119a54c0fbacc4e1fed15aed8ef0b1480
Ola <hollabodey.kilaso@gmail.com>
2023-06-13 19:30:34 +0100

    Made changes to the js file

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 let notifNum = document.querySelector(".notifNum");
 let markAll = document.getElementById("markAll");
-let messages = document.querySelectorAll(".message");
+let messages = document.querySelectorAll(".active");
+
+
+notifNum.innerHTML = messages.length;
+
+messages.forEach((message) => {
+    message.addEventListener("click", (e) => {
+        e.preventDefault();
+
+        message.classList.remove("active");
+        const newNotifNum = document.querySelectorAll(".active");
+        
+        notifNum.innerHTML = newNotifNum.length;
+    })
+})
+
 
 markAll.addEventListener("click", function(e){
     e.preventDefault();
@@ -12,25 +27,24 @@ markAll.addEventListener("click", function(e){
     notifNum.innerHTML = 0;
 });
 
-messages.forEach( (msg) => {
-    msg.addEventListener("click", function(e){
-        e.preventDefault();
+// messages.forEach( (msg) => {
+//     msg.addEventListener("click", function(e){
+//         e.preventDefault();
 
-        if (!messages.active) {
-            msg.classList.remove("active");
+//         if (!messages.active) {
+//             msg.classList.remove("active");
 
-            if (notifNum.innerHTML >= 1){
-                notifNum.innerHTML --;
-            } else if (notifNum.innerHTML <= 0){
-                notifNum.innerHTML = 0;
-            } else{
-                notifNum.innerHTML ++;
-            }
-        }
+//             if (notifNum.innerHTML >= 1){
+//                 notifNum.innerHTML --;
+//             } else if (notifNum.innerHTML <= 0){
+//                 notifNum.innerHTML = 0;
+//             } else{
+//                 notifNum.innerHTML ++;
+//             }
+//         }
 
-    });
-});
-
+//     });
+// });
 
 
 
